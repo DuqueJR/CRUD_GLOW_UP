2f5a7520c99195fdc9f2ca78d14157d650bed69f
grisales <juan.grisales65@eia.edu.co>
2025-11-03 22:03:40 -0500

    creacion logica para cambiar de vistas

---
+++ src/main/java/org/example/vistas/admin.java
@@ -0,0 +1,13 @@
+package org.example.vistas;
+
+import lombok.Getter;
+
+import javax.swing.*;
+
+public class admin {
+    private JButton Admin_agregarCategoriaButton;
+    private JButton Admin_agregarProductoButton;
+    private JButton Admin_principalButton1;
+    @Getter
+    private JPanel Admin_panel;
+}
---
+++ src/main/java/org/example/vistas/login.java
@@ -0,0 +1,15 @@
+package org.example.vistas;
+
+import lombok.Getter;
+
+import javax.swing.*;
+
+public class login {
+    private JPanel LoginFormulario;
+    private JTextField LoginUsuarioTextField;
+    private JPasswordField LoginContrasenaPasswordField;
+    private JButton enviarButton;
+    private JButton LoginCambiarRegister;
+    @Getter
+    private JPanel Login_panel;
+}
