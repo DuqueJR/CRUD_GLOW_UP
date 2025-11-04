b6cce9496cdfd2e5c91a42469d00dd71fd72e7ba
grisales <juan.grisales65@eia.edu.co>
2025-11-04 00:33:30 -0500

    prueba con la vista de login y bd

---
+++ src/main/java/org/example/vistas/login.java
@@ -0,0 +1,22 @@
+package org.example.vistas;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class login {
+    private JButton ingresarButton;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton button2;
+
+    public login() {
+
+        ingresarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+}
